6fafcabc7e8763eb25a3d7285de063b72004b487
GrahamTCreighton <guitarrista.1988@hotmail.com>
2022-08-10 16:35:46 +0200

    Creating Anchor story

---
+++ src/stories/Anchor.stories.jsx
@@ -0,0 +1,14 @@
+// Anchor.Stories.jsx
+
+import React from "react";
+
+import { Anchor } from "./Anchor";
+
+export default {
+  /* 👇 The title prop is optional.
+   * See https://storybook.js.org/docs/react/configure/overview#configure-story-loading
+   * to learn how to generate automatic titles
+   */
+  title: "Anchor",
+  component: Anchor,
+};
