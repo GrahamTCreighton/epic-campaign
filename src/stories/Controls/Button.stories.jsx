52d0270aac000b281f0a568aee5c1e5f5c05d35c
GrahamTCreighton <guitarrista.1988@hotmail.com>
2022-08-24 20:46:27 +0200

    Update Button.stories

--- src/stories/Controls/Button.stories.jsx
+++ src/stories/Controls/Button.stories.jsx
@@ -15,26 +15,87 @@ export default {
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
 const Template = (args) => <Button {...args} />;
 
-export const Primary = Template.bind({});
-// More on args: https://storybook.js.org/docs/react/writing-stories/args
-Primary.args = {
-  primary: true,
-  label: "Button",
+export const DefaultDefault = Template.bind({});
+DefaultDefault.args = {
+  label: "Default Default",
 };
 
-export const Secondary = Template.bind({});
-Secondary.args = {
-  label: "Button",
+export const DefaultPrimary = Template.bind({});
+DefaultPrimary.args = {
+  label: "Default Primary",
+};
+
+export const DefaultSecondary = Template.bind({});
+DefaultSecondary.args = {
+  label: "Default Secondary",
+};
+
+export const DefaultHighlight = Template.bind({});
+DefaultHighlight.args = {
+  label: "Default Highlight",
+};
+
+export const DefaultFill = Template.bind({});
+DefaultFill.args = {
+  label: "Default Fill",
+};
+
+export const PrimaryFill = Template.bind({});
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+PrimaryFill.args = {
+  primary: true,
+  label: "Primary Fill",
+};
+
+export const SecondaryFill = Template.bind({});
+SecondaryFill.args = {
+  label: "Secondary Fill",
+};
+
+export const SuccessFill = Template.bind({});
+SuccessFill.args = {
+  label: "Success Fill",
+};
+
+export const DangerFill = Template.bind({});
+DangerFill.args = {
+  label: "Danger Fill",
+};
+
+export const WarningFill = Template.bind({});
+WarningFill.args = {
+  label: "Warning Fill",
+};
+
+export const InfoFill = Template.bind({});
+InfoFill.args = {
+  label: "Info Fill",
+};
+
+export const OutlineDefault = Template.bind({});
+OutlineDefault.args = {
+  label: "Outline Default",
+};
+
+export const OutlinePrimary = Template.bind({});
+OutlinePrimary.args = {
+  label: "Outline Primary",
 };
 
 export const Large = Template.bind({});
 Large.args = {
   size: "large",
-  label: "Button",
+  label: "Large Button",
+};
+
+export const Medium = Template.bind({});
+Medium.args = {
+  size: "medium",
+  label: "Medium Button",
 };
 
 export const Small = Template.bind({});
 Small.args = {
   size: "small",
-  label: "Button",
+  label: "Small Button",
 };
